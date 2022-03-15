61d353de14717bd91e5dfd7c3c7fd6411865f061
Caleb Luster <cwl117@outlook.com>
2022-03-15 12:18:26 -0500

    added all the options for the initial inquirer prompt. Now I will creat mutiple functions to store all the questions for each one of the choices the user can select.

--- server.js
+++ server.js
@@ -11,6 +11,27 @@ database.connect(async function () {
 
 function startApp() {
   inquirer.prompt([
-      {}
-  ]);
+    {
+      type: "list",
+      name: "choice",
+      message: "What would you like to start with today?",
+      choices: [
+        "View Departments",
+        "View Employees",
+        "View Roles",
+        "Add Department",
+        "Add Employee",
+        "Add Role",
+        "Exit",
+      ],
+    },
+ ]).then((inquirerAnswers) => {
+     switch (inquirerAnswers.choice) {
+         
+     }
+ })
+
+
+
 }
+
