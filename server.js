75f1c7588922f45db28504bc14dfae1b4abff4e2
Caleb Luster <cwl117@outlook.com>
2022-03-15 12:24:50 -0500

    added function and comments right now as oppose to at the end. Hopefully this will help productivity

--- server.js
+++ server.js
@@ -10,28 +10,55 @@ database.connect(async function () {
 });
 
 function startApp() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What would you like to start with today?",
-      choices: [
-        "View Departments",
-        "View Employees",
-        "View Roles",
-        "Add Department",
-        "Add Employee",
-        "Add Role",
-        "Exit",
-      ],
-    },
- ]).then((inquirerAnswers) => {
-     switch (inquirerAnswers.choice) {
-         
-     }
- })
-
-
-
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to start with today?",
+        choices: [
+          "View Departments",
+          "View Employees",
+          "View Roles",
+          "Add Department",
+          "Add Employee",
+          "Add Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((inquirerAnswers) => {
+      switch (inquirerAnswers.choice) {
+      }
+    });
 }
 
+// /////////////////////////////////////////////////////////////
+// function for viewing all departments.
+// /////////////////////////////////////////////////////////////
+function isDepartments() {}
+
+// /////////////////////////////////////////////////////////////
+// function for viewing all employees
+// /////////////////////////////////////////////////////////////
+function isEmployees() {}
+
+// /////////////////////////////////////////////////////////////
+// function for viewing all roles
+// /////////////////////////////////////////////////////////////
+function isRoles() {}
+
+// /////////////////////////////////////////////////////////////
+// function for adding department
+// /////////////////////////////////////////////////////////////
+function addDepartment() {}
+
+// /////////////////////////////////////////////////////////////
+// function for adding employees
+// /////////////////////////////////////////////////////////////
+function addEmployee() {}
+
+// /////////////////////////////////////////////////////////////
+// function for adding role
+// /////////////////////////////////////////////////////////////
+function addRole() {}
